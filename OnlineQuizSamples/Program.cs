621611059c1bd88affaa01461d7f1cd0da851d2f
santhoshtns <san.tns@gmail.com>
2018-12-20 20:27:07 +0800

    Add IsPesel

--- OnlineQuizSamples/Program.cs
+++ OnlineQuizSamples/Program.cs
@@ -6,7 +6,43 @@ namespace OnlineQuizSamples
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            IsPESEL();
+        }
+
+        public static void IsPESEL()
+        {
+            var myInput = Console.ReadLine().Trim();
+            Console.WriteLine(myInput);
+
+            if (string.IsNullOrEmpty(myInput))
+            {
+                return;
+            }
+
+            var count = Convert.ToInt16(myInput);
+
+            while (count > 0)
+            {
+                myInput = Console.ReadLine().Trim();
+                if (myInput.Length != 11)
+                {
+                    Console.WriteLine("N");
+                }
+                else
+                {
+                    var sum = myInput[0].ToInt() + myInput[10].ToInt() +
+                        myInput[4].ToInt() + myInput[8].ToInt() +
+                        (myInput[1].ToInt() + myInput[9].ToInt() + myInput[5].ToInt()) * 3 +
+                        (myInput[2].ToInt() + myInput[6].ToInt()) * 7 +
+                        (myInput[3].ToInt() + myInput[7].ToInt()) * 9;
+                    if (sum % 10 == 0)
+                        Console.WriteLine("Y");
+                    else
+                        Console.WriteLine("N");
+                }
+                count--;
+            }
         }
     }
 }
