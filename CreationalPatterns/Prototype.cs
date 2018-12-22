7ff4e35474f60a3974311abc659697b3e0df9eb6
Santhosh Sampath <san.tns@gmail.com>
2018-12-22 21:13:22 +0800

    Add prototype

---
+++ CreationalPatterns/Prototype.cs
@@ -0,0 +1,23 @@
+﻿namespace CreationalPatterns
+{
+    public abstract class Prototype
+    {
+        public abstract Prototype Clone();
+    }
+
+    public class ConcretePrototype1 : Prototype
+    {
+        public override Prototype Clone()
+        {
+            return (Prototype)this.MemberwiseClone();
+        }
+    }
+
+    public class ConcretePrototype2 : Prototype
+    {
+        public override Prototype Clone()
+        {
+            return (Prototype)this.MemberwiseClone();
+        }
+    }
+}
