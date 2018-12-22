a4e10176c3002ae32abb80ef19e0ee02473a6ea3
Santhosh Sampath <san.tns@gmail.com>
2018-12-22 20:54:20 +0800

    added builder

---
+++ CreationalPatterns/Builder.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CreationalPatterns
+{
+    public interface IBuilder
+    {
+        string RunBuilderTask1();
+        string RunBuilderTask2();
+    }
+
+    class Builder1 : IBuilder
+    {
+        public string RunBuilderTask1()
+        {
+            throw new ApplicationException("Task1");
+        }
+
+        public string RunBuilderTask2()
+        {
+            throw new ApplicationException("Task2");
+        }
+    }
+
+    class Builder2 : IBuilder
+    {
+        public string RunBuilderTask1()
+        {
+            return "Task3";
+        }
+
+        public string RunBuilderTask2()
+        {
+            return "Task4";
+        }
+    }
+
+    public class Director
+    {
+        public IBuilder CreateBuilder(int type)
+        {
+            IBuilder builder = null;
+            switch (type)
+            {
+                case 1:
+                    builder = new Builder1();
+                    break;
+                case 2:
+                    builder = new Builder2();
+                    break;
+                default:
+                    break;
+            }
+            builder.RunBuilderTask1();
+            builder.RunBuilderTask2();
+            return builder;
+        }
+    }
+}
