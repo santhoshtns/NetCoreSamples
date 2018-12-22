5c4722f31fe3198bf521ce496d3ab17ffc6c3142
Santhosh Sampath <san.tns@gmail.com>
2018-12-22 15:48:14 +0800

    Add factory

---
+++ CreationalPatterns/Factory.cs
@@ -0,0 +1,49 @@
+﻿namespace CreationalPatterns
+{
+    public enum PeopleType
+    {
+        RURAL,
+        URBAN
+    }
+
+    public interface IPeople
+    {
+        string GetName();
+    }
+
+    public class Factory
+    {
+        public IPeople GetPeople(PeopleType peopleType)
+        {
+            IPeople people = null;
+            switch (peopleType)
+            {
+                case PeopleType.RURAL:
+                    people = new Villager();
+                    break;
+                case PeopleType.URBAN:
+                    people = new UrbanPeople();
+                    break;
+                default:
+                    break;
+            }
+            return people;
+        }
+    }
+
+    internal class UrbanPeople : IPeople
+    {
+        public string GetName()
+        {
+            return "Urban Guy";
+        }
+    }
+
+    internal class Villager : IPeople
+    {
+        public string GetName()
+        {
+            return "Village Guy";
+        }
+    }
+}
