85b3a307bf3ede7b7edaadf5ceda55ac0021f607
Santhosh Sampath <san.tns@gmail.com>
2018-12-22 15:22:27 +0800

    Added Singleton Pattern

---
+++ CreationalPatterns/Singleton.cs
@@ -0,0 +1,24 @@
+﻿namespace CreationalPatterns
+{
+    public class Singleton
+    {
+        private static Singleton instance = null;
+
+        private Singleton()
+        {
+
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Singleton();
+                }
+                return instance;
+            }
+        }
+    }
+}
