a2631793d5941e1bf7a6a28364368c9b61721659
Santhosh Sampath <san.tns@gmail.com>
2018-12-23 15:36:30 +0800

    Add Structural Patterns. with Adapter Pattern

---
+++ StructuralPatterns/Adapter.cs
@@ -0,0 +1,25 @@
+﻿namespace StructuralPatterns
+{
+    internal interface IAdapter
+    {
+        void Add();
+    }
+
+    public class ToClass
+    {
+        public void Push()
+        {
+
+        }
+    }
+
+    public class FromClass : IAdapter
+    {
+        private ToClass _toClass = new ToClass();
+
+        public void Add()
+        {
+            _toClass.Push();
+        }
+    }
+}
---
+++ StructuralPatterns/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StructuralPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
