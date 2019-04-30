d109a6fd92e9a2b9554c7b1671a14725f2682413
Santhosh Sampath <san.tns@gmail.com>
2019-05-01 01:35:54 +0800

    BinaryGap added

---
+++ BinaryGap/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryGap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            solution(32);
+        }
+
+        public static int solution(int N)
+        {
+            // write your code in C# 6.0 with .NET 4.5 (Mono)
+            string binary = Convert.ToString(N, 2);
+            var arr = binary.Split('1').ToList();
+            arr.RemoveAt(arr.Count - 1);
+            return arr.OrderByDescending(a => a.Length).First().Length;
+        }
+    }
+}
