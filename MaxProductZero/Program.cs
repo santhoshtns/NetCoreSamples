5ede7b25ceafe1872f97bc4abf6b5bb1894a227b
Santhosh Sampath <san.tns@gmail.com>
2019-04-30 23:31:53 +0800

    optimized MaxProductZero

--- MaxProductZero/Program.cs
+++ MaxProductZero/Program.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 
 namespace MaxProductZero
 {
@@ -7,7 +6,7 @@ namespace MaxProductZero
     {
         static void Main(string[] args)
         {
-            solution(new[] { 7, 15, 6 });
+            solution(new[] { 1, 2, 1, 1 });
         }
 
         public static int solution(int[] A)
@@ -19,18 +18,31 @@ namespace MaxProductZero
                 return 0;
             }
 
-            var temp = A.Where(x => x % 2 == 0 || x % 5 == 0).ToArray();
+            var temp = A.Where(x => x >= 1 && x <= 1000000000 &&
+            (x % 2 == 0 || x % 5 == 0)).ToArray();
             if (temp.Length >= 3)
                 A = temp;
+            else
+            {
+                ulong product = 1;
+                for (int i = 0; i < temp.Length; i++)
+                {
+                    product *= (ulong)temp[i];
+                }
+                int remainder = 0;
+                return findTrailingZeros(product, ref remainder);
+            }
 
             for (int a = 0; a < A.Length - 2; a++)
             {
                 for (int b = a + 1; b < A.Length - 1; b++)
                 {
-                    for (int c = b + 1; c < A.Length && c != b; c++)
+                    for (int c = b + 1; c < A.Length; c++)
                     {
-                        var product = (A[a] * A[b] * A[c]);
-                        var zeroes = findTrailingZeros(product);
+                        ulong product = ((ulong)A[a] * (ulong)A[b]);
+                        int remainder = 0;
+                        var zeroes = findTrailingZeros(product, ref remainder);
+                        zeroes += findTrailingZeros(((ulong)remainder * (ulong)A[c]), ref remainder);
                         if (zeroes > maxZeroCount)
                             maxZeroCount = zeroes;
                     }
@@ -40,51 +52,56 @@ namespace MaxProductZero
             return maxZeroCount;
         }
 
-        public static int solution_old1(int[] A)
+        //public static int solution_old1(int[] A)
+        //{
+        //    // write your code in C# 6.0 with .NET 4.5 (Mono)
+        //    int maxZeroCount = 0;
+        //    if (A.Length < 3 && A.Length > 100000)
+        //    {
+        //        return 0;
+        //    }
+
+        //    var temp = A.Where(x => x % 2 == 0 || x % 5 == 0).ToArray();
+        //    if (temp.Length >= 3)
+        //        A = temp;
+
+        //    List<System.Tuple<int, int, int>> combinations = new List<System.Tuple<int, int, int>>();
+        //    for (int a = 0; a < A.Length - 2; a++)
+        //    {
+        //        for (int b = a + 1; b < A.Length - 1; b++)
+        //        {
+        //            for (int c = b + 1; c < A.Length && c != b; c++)
+        //            {
+        //                combinations.Add(System.Tuple.Create(A[a], A[b], A[c]));
+        //            }
+        //        }
+        //    }
+
+        //    foreach (var triplet in combinations)
+        //    {
+        //        var product = triplet.Item1 * triplet.Item2 * triplet.Item3;
+        //        var zeroes = findTrailingZeros(product);
+        //        if (zeroes > maxZeroCount)
+        //            maxZeroCount = zeroes;
+        //    }
+
+        //    return maxZeroCount;
+        //}
+
+        static int findTrailingZeros(ulong n, ref int remainder)
         {
-            // write your code in C# 6.0 with .NET 4.5 (Mono)
-            int maxZeroCount = 0;
-            if (A.Length < 3 && A.Length > 100000)
+            if (n == 0)
             {
+                remainder = 0;
                 return 0;
             }
-
-            var temp = A.Where(x => x % 2 == 0 || x % 5 == 0).ToArray();
-            if (temp.Length >= 3)
-                A = temp;
-
-            List<System.Tuple<int, int, int>> combinations = new List<System.Tuple<int, int, int>>();
-            for (int a = 0; a < A.Length - 2; a++)
-            {
-                for (int b = a + 1; b < A.Length - 1; b++)
-                {
-                    for (int c = b + 1; c < A.Length && c != b; c++)
-                    {
-                        combinations.Add(System.Tuple.Create(A[a], A[b], A[c]));
-                    }
-                }
-            }
-
-            foreach (var triplet in combinations)
-            {
-                var product = triplet.Item1 * triplet.Item2 * triplet.Item3;
-                var zeroes = findTrailingZeros(product);
-                if (zeroes > maxZeroCount)
-                    maxZeroCount = zeroes;
-            }
-
-            return maxZeroCount;
-        }
-
-        static int findTrailingZeros(int n)
-        {
-            if (n == 0) return 0;
             int count = 0;
             while (n % 10 == 0)
             {
-                n = n / 10;
+                n /= 10;
                 count++;
             }
+            remainder = (int)(n);
             return count;
         }
     }
