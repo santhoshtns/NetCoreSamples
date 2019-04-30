1ba2f6246f365b79ce1af250c0ba726497aa8a08
Santhosh Sampath <san.tns@gmail.com>
2019-04-30 23:51:44 +0800

    Add TheaterTickets Project

---
+++ TheaterTickets/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace TheaterTickets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(solution(new[] { 2, 2, 1, 2, 2 }));
+        }
+
+        public static int solution(int[] A)
+        {
+            // write your code in C# 6.0 with .NET 4.5 (Mono)
+            if (A == null || A.Length < 3)
+            {
+                return 0;
+            }
+
+            List<string> combinations = new List<string>();
+
+            for (int a = 0; a < A.Length - 2; a++)
+            {
+                for (int b = a + 1; b < A.Length - 1; b++)
+                {
+                    for (int c = b + 1; c < A.Length; c++)
+                    {
+                        var combination = $"{A[a]},{A[b]},{A[c]}";
+                        if (!combinations.Contains(combination))
+                        {
+                            combinations.Add(combination);
+                        }
+                    }
+                }
+            }
+
+            return combinations.Count;
+        }
+    }
+}
