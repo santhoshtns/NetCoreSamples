ae002dd6643493b4c8754ef0b6dc30ed29631219
Santhosh Sampath <san.tns@gmail.com>
2019-05-11 00:15:00 +0800

    Quick Sort

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            new Arrays().RearrangeArray();
+            int[] arr = new int[] { 2, 5, -4, 11, 0, 18, 22, 67, 51, 6 };
+            QuickSort.Quick_Sort(arr, 0, arr.Length - 1);
+
+            //new Arrays().RearrangeArray();
             //new Arrays().MergeTwosortedArray();
             //new Arrays().FirstNonRepeatingNumber();
             //new Arrays().FirstAndSecondSmallest();
