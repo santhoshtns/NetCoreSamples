658aeb2f29f7b57347b4aeabbb7270c99a709326
santhoshts <santhosh.sampath@cxagroup.com>
2019-06-13 18:23:53 +0800

    Added Age Calculate

--- SimpleCheck/Program.cs
+++ SimpleCheck/Program.cs
@@ -7,6 +7,11 @@ namespace SimpleCheck
     {
         static void Main(string[] args)
         {
+            int currentAge = CalculateCurrentAge(new DateTime(1984, 06, 02));
+
+            string benefitXml = "abc";
+            string benefitXml2 = benefitXml.Replace("c", "DEF");
+
             DateFormats();
             var flag = true;
             if (!bool.TryParse(ConfigurationManager.AppSettings["Setting21"],
@@ -20,8 +25,18 @@ namespace SimpleCheck
 
         private static void DateFormats()
         {
-            DateTime dt = new DateTime(1987,12,31);
+            DateTime dt = new DateTime(1987, 12, 31);
             Console.WriteLine(dt.ToString("d MMM yyyy"));
+        }
+
+        private static int CalculateCurrentAge(DateTime birthDate)
+        {
+            int age = 0;
+            age = DateTime.Now.Year - birthDate.Year;
+            if (DateTime.Now.DayOfYear < birthDate.DayOfYear)
+                age = age - 1;
+
+            return age;
         }
     }
 }
