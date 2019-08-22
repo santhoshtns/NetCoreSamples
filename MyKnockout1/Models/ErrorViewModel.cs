b3ba7b040265455a73173c4fb047a277556d00cd
Santhosh Sampath <santhosh@chinsay.com>
2019-08-22 13:19:29 +0800

    add knockoutjs sample

---
+++ MyKnockout1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyKnockout1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
