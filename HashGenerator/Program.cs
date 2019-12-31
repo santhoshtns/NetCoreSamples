cf3001b3291fe8a47e95289ad80fb2456e776460
Santhosh Sampath <san.tns@gmail.com>
2019-12-31 10:15:10 +0800

    hash generate from guid

---
+++ HashGenerator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HashGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var newGuid = Guid.NewGuid().ToString();
+
+            var convertedGuidToBytes = System.Text.Encoding.UTF8.GetBytes(newGuid);
+            var mySha256 = System.Security.Cryptography.SHA256.Create();
+            var toString = Convert.ToBase64String(mySha256.ComputeHash(convertedGuidToBytes));
+
+            Console.WriteLine(newGuid);
+            Console.WriteLine(toString);
+        }
+    }
+}
