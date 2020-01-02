e529c2bae29f7d4ac13a539eb7447d89bfd3e247
Santhosh Sampath <san.tns@gmail.com>
2020-01-02 14:33:46 +0800

    Add Password hash generator

---
+++ PasswordHashGenerator/HashValidatorRfc2898.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace PasswordHashGenerator
+{
+    public static class HashValidatorRfc2898
+    {
+        private const int Iterations = 1000;
+        private const int HashSize = 32;
+        private const int SaltSize = 32;
+
+        public static bool ValidatePassword(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            var rdb = new Rfc2898DeriveBytes(password, passwordSalt, Iterations);
+
+            var hash = rdb.GetBytes(HashSize);
+            return IsSameByteArray(passwordHash, hash);
+        }
+
+        public static void HashPassword(string password, out byte[] salt, out byte[] hash)
+        {
+            var rdb = new Rfc2898DeriveBytes(password, SaltSize, Iterations);
+
+            salt = rdb.Salt;
+            hash = rdb.GetBytes(HashSize);
+        }
+
+        private static bool IsSameByteArray(IReadOnlyCollection<byte> a, IReadOnlyList<byte> b)
+        {
+            if (a.Count != b.Count)
+            {
+                return false;
+            }
+
+            return !a.Where((t, i) => t != b[i]).Any();
+        }
+    }
+}
---
+++ PasswordHashGenerator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PasswordHashGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var password = args.Length > 0 ? args[0] : Console.ReadLine();
+            HashValidatorRfc2898.HashPassword(password, out var salt, out var hash);
+            Console.WriteLine($"Password: {password}");
+            Console.WriteLine($"PasswordHash: 0x{hash.ConvertByteToString()}");
+            Console.WriteLine($"PasswordSalt: 0x{salt.ConvertByteToString()}");
+            Console.Read();
+        }
+    }
+
+    public static class Extensions
+    {
+        public static string ConvertByteToString(this byte[] data)
+        {
+            return BitConverter.ToString(data).Replace("-", string.Empty);
+        }
+    }
+}
