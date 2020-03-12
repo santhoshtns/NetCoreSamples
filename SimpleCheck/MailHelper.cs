7ac1114de7bf19503090c59b508d9c767e7b80bb
Santhosh Sampath <san.tns@gmail.com>
2020-03-12 14:34:42 +0800

    mail address validator

---
+++ SimpleCheck/MailHelper.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+
+namespace SimpleCheck
+{
+    class MailHelper
+    {
+        public string WildcardCharacter { get; } = "*";
+
+        public bool WhiteListContainsExactOrWildcardedEmail(string address, IEnumerable<string> whiteList)
+        {
+            if (string.IsNullOrEmpty(address) || whiteList == null ||
+                !ConvertToEmailAddress(address, out var emailAddress))
+            {
+                return false;
+            }
+
+            foreach (var item in whiteList.ToList())
+            {
+                if (item.StartsWith(WildcardCharacter) &&
+                    ContainsWildcardedEmail(emailAddress.Address, item))
+                {
+                    return true;
+                }
+
+                if (WhiteListContainsExactEmail(emailAddress.Address, item))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static bool ConvertToEmailAddress(string email, out MailAddress mailAddress)
+        {
+            try
+            {
+                mailAddress = new MailAddress(email);
+            }
+            catch (Exception) //Invalid Email
+            {
+                mailAddress = null;
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool ContainsWildcardedEmail(string address, string whiteListItem)
+        {
+            return address.ToLower().EndsWith(whiteListItem.ToLower().Replace(WildcardCharacter, ""));
+        }
+
+        private bool WhiteListContainsExactEmail(string address, string whiteListItem)
+        {
+            return address.ToLower().Equals(whiteListItem.ToLower());
+        }
+    }
+}
--- SimpleCheck/Program.cs
+++ SimpleCheck/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Configuration;
 using System.Globalization;
+using System.Net.Mail;
 
 namespace SimpleCheck
 {
@@ -8,6 +10,24 @@ namespace SimpleCheck
     {
         static void Main(string[] args)
         {
+            List<string> emaiList = new List<string>();
+            emaiList.Add("Santhosh <santhosh@gmail.com>");
+            emaiList.Add("Santhosh<#santhosh@gmail.com>");
+            emaiList.Add("<santhosh@gmail.com>");
+            emaiList.Add("@gmail");
+
+            List<string> whiteList = new List<string>();
+            whiteList.Add("*");
+
+            MailHelper mailHelper = new MailHelper();
+
+            foreach (var email in emaiList)
+            {
+                //var emailAddress = ConvertToMailAddress(email);
+                Console.WriteLine($"{email} IsValid:{mailHelper.WhiteListContainsExactOrWildcardedEmail(email, whiteList)}");
+            }
+
+
             GetDataPointValueAsString(null);
 
             int currentAge = CalculateCurrentAge(new DateTime(1984, 06, 02));
@@ -26,9 +46,24 @@ namespace SimpleCheck
             Console.WriteLine(flag);
         }
 
+        public static MailAddress ConvertToMailAddress(string email)
+        {
+            MailAddress mailAddress = null;
+            try
+            {
+                mailAddress = new MailAddress(email);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            return mailAddress;
+        }
+
         private static string GetDataPointValueAsString(object dpValue)
         {
-            if(dpValue is DateTime dtValue)
+            if (dpValue is DateTime dtValue)
             {
                 return dtValue.ToString("yyyy-MM-dd HH:mm:ss");
             }
