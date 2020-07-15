664609cb29de74d6658a64f48fc985efddc5e29e
Santhosh Sampath <san.tns@gmail.com>
2020-07-15 23:41:20 +0800

    testing operators

---
+++ Operators/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        [Flags]
+        public enum BaseState
+        {
+            None = 0,
+            Inactive = 1 << 0, //1
+            Deleted = 1 << 1, //2
+            ReadOnly = 1 << 2, //4
+            Locked = 1 << 3, //8
+            Draft = 1 << 4 //16
+        }
+
+        static void Main(string[] args)
+        {
+            Guid? testGuid = null;
+
+            Console.WriteLine(testGuid.Value.ToString());
+
+
+            Console.WriteLine("Hello World!");
+
+            BaseState? baseState = null;
+
+            //baseState = baseState | BaseState.Deleted ?? BaseState.Deleted;
+
+            BaseState? excludeStates = baseState | BaseState.Deleted ?? BaseState.Deleted;
+            excludeStates ??= BaseState.None;
+
+        }
+    }
+}
