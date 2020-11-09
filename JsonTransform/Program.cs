4a842a96029d7a3bb2408f32a455c1a03a4bb0e9
Santhosh Sampath <san.tns@gmail.com>
2020-11-09 15:49:52 +0800

    sample json transformer

---
+++ JsonTransform/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace JsonTransform
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = File.ReadAllText("Input.json");
+            string transformer = File.ReadAllText("Transformer.json"); ;
+            var jsonTransformer = new JUST.JsonTransformer();
+            string transformedString = jsonTransformer.Transform(transformer, input);
+            Console.WriteLine(transformedString);
+        }
+    }
+}
