3add9a796b7ae655af698b4f8ae2dd914ce26f77
Santhosh Sampath <san.tns@gmail.com>
2022-04-01 00:07:06 +0800

    add test app

---
+++ WpfOpenCloseBrowser/BrowserWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows;
+
+namespace WpfOpenCloseBrowser
+{
+    /// <summary>
+    /// Interaction logic for BrowserWindow.xaml
+    /// </summary>
+    public partial class BrowserWindow : Window
+    {
+        private string _url;
+
+        public BrowserWindow(string url)
+        {
+            _url = url;
+            InitializeComponent();
+            txtUrl.Text = _url;
+        }
+
+        private void BrowserWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            WebView2Browser.Source = new Uri(_url);
+        }
+    }
+}
---
+++ WpfOpenCloseBrowser/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfOpenCloseBrowser
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<BrowserWindow> _browserWindows = new List<BrowserWindow>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenBrowser_OnClick(object sender, RoutedEventArgs e)
+        {
+            var browserWindow = new BrowserWindow(NavigateUrl.Text);
+            _browserWindows.Add(browserWindow);
+            browserWindow.Show();
+        }
+
+        private void CloseBrowser_OnClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (_browserWindows.Count > 0)
+                {
+                    var browserWindow = _browserWindows.First();
+                    _browserWindows.Remove(browserWindow);
+                    browserWindow.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
