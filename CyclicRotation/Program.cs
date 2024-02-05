298a114099a17e708632e45735624f6652cc72ad
Santhosh Sampath <santhosh.sampath@sea.live>
2024-02-05 08:12:28 +0530

    cyclic rotation

---
+++ CyclicRotation/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace CyclicRotation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = solution(new[] { 1, 2, 3, 4, 5 }, 8);
+            Console.WriteLine("");
+
+        }
+
+        public static int[] solution(int[] A, int K)
+        {
+            var shift = K % A.Length;
+            var r = new int[A.Length];
+            if (r.Length > 0)
+            {
+                for (int i = -1, j = A.Length - shift; ++i < A.Length;)
+                    r[i] = A[(j + i) % A.Length];
+            }
+
+            return r;
+        }
+    }
+}
