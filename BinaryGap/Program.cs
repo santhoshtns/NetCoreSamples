b59cd1582ea1d7a8c5f55b2e8a9db76d724800ce
Santhosh Sampath <santhosh.sampath@sea.live>
2024-02-05 07:06:23 +0530

    update binarygap

--- BinaryGap/Program.cs
+++ BinaryGap/Program.cs
@@ -10,16 +10,35 @@ namespace BinaryGap
     {
         static void Main(string[] args)
         {
-            solution(32);
+            Console.WriteLine(solution(1041));
+            Console.WriteLine(solution(32));
         }
 
         public static int solution(int N)
         {
             // write your code in C# 6.0 with .NET 4.5 (Mono)
-            string binary = Convert.ToString(N, 2);
-            var arr = binary.Split('1').ToList();
-            arr.RemoveAt(arr.Count - 1);
-            return arr.OrderByDescending(a => a.Length).First().Length;
+            //string binary = Convert.ToString(N, 2);
+            //var arr = binary.Split('1').ToList();
+            //arr.RemoveAt(arr.Count - 1);
+            //return arr.OrderByDescending(a => a.Length).First().Length;
+            bool started = false;
+            int length = 0, max = 0;
+            for (uint i = 1; i <= N; i <<= 1)
+            {
+                if ((i & N) != 0)
+                {
+                    if (started)
+                    {
+                        if (length > max)
+                            max = length;
+                        length = 0;
+                    }
+                    started = true;
+                }
+                else if (started)
+                    ++length;
+            }
+            return max;
         }
     }
 }
--- BinaryGap/Properties/AssemblyInfo.cs
+++ BinaryGap/Properties/AssemblyInfo.cs
@@ -1,16 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("BinaryGap")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BinaryGap")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,16 +11,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("3d5f2d92-758c-4248-b640-c39c6ca3b518")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
